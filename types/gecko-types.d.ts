92ca110a7c92dca230881ef06d2787d0a77dfe95
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-01-30 20:46:18 +1100

    Provide root `.d.ts` file

---
+++ types/gecko-types.d.ts
@@ -0,0 +1,5 @@
+///<reference path="./custom/idlTypes.d.ts" />
+///<reference path="./custom/webidlTypes.d.ts" />
+
+///<reference path="./gen/idls.d.ts" />
+///<reference path="./gen/namespaces.d.ts" />
