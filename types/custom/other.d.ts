78b09e1ad1b337c466dde7815986409bd6058dea
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-02-07 17:36:11 +1100

    ✨ Add `docShell` global

---
+++ types/custom/other.d.ts
@@ -0,0 +1,3 @@
+export declare global {
+  const docShell: nsIDocShellType
+}
--- types/gecko-types.d.ts
+++ types/gecko-types.d.ts
@@ -1,6 +1,7 @@
 ///<reference path="./custom/ChromeUtils.d.ts" />
 ///<reference path="./custom/Components.d.ts" />
 ///<reference path="./custom/idlTypes.d.ts" />
+///<reference path="./custom/other.d.ts" />
 ///<reference path="./custom/webidlTypes.d.ts" />
 ///<reference path="./custom/xulElements.d.ts" />
 
