883fd6e7a2e5b8d8c271789291d43423971f8e9f
trickypr <23250792+trickypr@users.noreply.github.com>
2023-11-14 20:43:02 +1100

    🏷️ Exception type that just has data

--- types/custom/Components.d.ts
+++ types/custom/Components.d.ts
@@ -18,22 +18,33 @@ declare module Components {
    * @see Outdated: {@link https://udn.realityripple.com/docs/Mozilla/Tech/XPCOM/Language_Bindings/Components.Exception}
    * @see Cleanup bug: {@link https://bugzilla.mozilla.org/show_bug.cgi?id=1435483}
    */
-  declare function Exception(
+  declare function Exception<T>(
     message?: string,
     result?: nsresult,
     stack?: unknown,
-    data?: unknown,
+    data?: T
   ): nsIExceptionType
+
+  /**
+   * Components.Exception is a JavaScript constructor to create nsIException objects. These
+   * exception objects may be thrown when implementing xpcom interfaces in JavaScript, and they
+   * can provide better diagnostics in the error console if not caught than simply throwing an
+   * nsresult's value will.
+   *
+   * @see Outdated: {@link https://udn.realityripple.com/docs/Mozilla/Tech/XPCOM/Language_Bindings/Components.Exception}
+   * @see Cleanup bug: {@link https://bugzilla.mozilla.org/show_bug.cgi?id=1435483}
+   */
+  declare function Exception<T>(message?: string, data?: T): nsIExceptionType
 
   declare function Constructor<
     ContractId extends keyof typeof Cc,
     InterfaceName extends (typeof Cc)[ContractId]['interfaceName'],
     Interface extends CiMap[InterfaceName],
-    Initializer extends keyof Interface,
+    Initializer extends keyof Interface
   >(
     contractId: ContractId,
     interfaceName: InterfaceName,
-    initializer: Initializer,
+    initializer: Initializer
   ): {
     new (...params: Parameters<Interface[Initializer]>): Interface
   }
