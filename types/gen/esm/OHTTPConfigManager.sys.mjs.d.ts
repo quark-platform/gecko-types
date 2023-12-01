3e535488ede5d1a1a666e4b24e7a0d81e84d27ab
trickypr <23250792+trickypr@users.noreply.github.com>
2023-12-01 17:16:11 +1100

    :arrow_up: Bump gecko index

--- types/gen/esm/OHTTPConfigManager.sys.mjs.d.ts
+++ types/gen/esm/OHTTPConfigManager.sys.mjs.d.ts
@@ -1,6 +1,6 @@
 declare module "resource://gre/modules/OHTTPConfigManager.sys.mjs" {export class OHTTPConfigManager {
     static get(aURL: any, aOptions?: {}): Promise<Uint8Array>;
-    static "__#846284@#getInternal"(aURL: any, aOptions?: {}): Promise<any>;
+    static "__#846290@#getInternal"(aURL: any, aOptions?: {}): Promise<any>;
     static fetchAndStore(aURL: any, aOptions?: {}): Promise<ArrayBuffer>;
 }
 }
