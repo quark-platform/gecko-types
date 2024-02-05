52c36ba63a5a2d4e764619e43173bc08a0e79430
trickypr <23250792+trickypr@users.noreply.github.com>
2024-02-05 19:06:13 +1100

    :bug: Should be wrapped in `FileUtils`

--- types/custom/FileUtils.d.ts
+++ types/custom/FileUtils.d.ts
@@ -1,3 +1,5 @@
 declare module 'resource://gre/modules/FileUtils.sys.mjs' {
-  const File: new (path: string) => nsIFileType
+  export namespace FileUtils {
+    const File: new (path: string) => nsIFileType
+  }
 }
